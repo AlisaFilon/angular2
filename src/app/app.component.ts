676b4f2ba7c04127f6a027904ec58aea23e37b42
AlisaFilon <filonenkoalisa@gmail.com>
2021-11-14 23:06:18 +0100

    proba calc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular2';
+  title = 'My App Calculator';
+
+  public num1:any
+  public num2:any
+  public num3:any
+
+  add(){
+    this.num3=this.num1+this.num2;
+  }
+  sub(){
+    this.num3=this.num1-this.num2;
+  }
+  multiply(){
+    this.num3=this.num1*this.num2;
+  }
+  divide(){
+    this.num3=this.num1/this.num2;
+  }
+
 }
